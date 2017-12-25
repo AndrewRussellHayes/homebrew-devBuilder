a5bcc98310ecb860985e5e734f7761164ae5fc2a
IQU408 <Andrew.Hayes@capitalone.com>
2017-12-25 02:34:25 -0600

    check spacing?

--- formula/devbuilder.rb
+++ formula/devbuilder.rb
@@ -2,7 +2,7 @@ class Devbuilder < Formula
   desc "build a dev environment"
   homepage "https://github.com/AndrewRussellHayes/devBuilder"
 
-url "https://github.com/AndrewRussellHayes/devBuilder/releases/download/v0.0.1/devbuilder"
+  url "https://github.com/AndrewRussellHayes/devBuilder/releases/download/v0.0.1/devbuilder"
 
   version "0.0.1"
 	sha256 "f7d3f72d97f5cb85b4ff328b33e1d7fcbe2974de87558ae9981bab2de9afb4f6"
