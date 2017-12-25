98f3af795430f6162bcc9403e3d3e31817f8809f
IQU408 <Andrew.Hayes@capitalone.com>
2017-12-25 02:33:22 -0600

    removed everything

--- formula/devbuilder.rb
+++ formula/devbuilder.rb
@@ -7,10 +7,6 @@ url "https://github.com/AndrewRussellHayes/devBuilder/releases/download/v0.0.1/d
   version "0.0.1"
 	sha256 "f7d3f72d97f5cb85b4ff328b33e1d7fcbe2974de87558ae9981bab2de9afb4f6"
 
-  depends_on "curl"
-
-	bottle :unneeded
-
   def install
   end
 
