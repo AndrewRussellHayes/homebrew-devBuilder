d9430e8e9473ca1845fa86a697eea37bf0b01381
IQU408 <Andrew.Hayes@capitalone.com>
2017-12-25 19:30:56 -0600

    3 again

--- formula/devbuilder@3.rb
+++ formula/devbuilder@3.rb
@@ -2,8 +2,8 @@ class DevbuilderAT3 < Formula
   desc "build a dev environment"
   homepage "https://github.com/AndrewRussellHayes/devBuilder"
 
-  version "0.0.3"
-	url "https://github.com/AndrewRussellHayes/devBuilder/releases/download/v0.0.3/devbuilder"
+  version "3.0.3"
+	url "https://github.com/AndrewRussellHayes/devBuilder/releases/download/v3.0.3/devbuilder"
 	sha256 "8918bf831445c8630ab26009f05da4e2fa4800351941cd7348039671f862b83e"
 
   def install
