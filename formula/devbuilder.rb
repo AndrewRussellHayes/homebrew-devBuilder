299b2b2920e298b3d428f1dcc0dd2fe37ba11923
IQU408 <Andrew.Hayes@capitalone.com>
2017-12-25 19:14:36 -0600

    v2?

--- formula/devbuilder.rb
+++ formula/devbuilder.rb
@@ -2,10 +2,19 @@ class Devbuilder < Formula
   desc "build a dev environment"
   homepage "https://github.com/AndrewRussellHayes/devBuilder"
 
-  url "https://github.com/AndrewRussellHayes/devBuilder/releases/download/v0.0.1/devbuilder"
-
   version "0.0.1"
+  url "https://github.com/AndrewRussellHayes/devBuilder/releases/download/v0.0.1/devbuilder"
 	sha256 "8918bf831445c8630ab26009f05da4e2fa4800351941cd7348039671f862b83e"
+
+
+	#The devel spec (activated by passing --devel) is used for a project’s unstable releases. It is specified in a block:
+	devel do
+
+    version "0.0.2"
+		url "https://github.com/AndrewRussellHayes/devBuilder/releases/download/v0.0.2/devbuilder"
+		sha256 "8918bf831445c8630ab26009f05da4e2fa4800351941cd7348039671f862b83e"
+		
+	end
 
   def install
 		bin.install "devbuilder"
